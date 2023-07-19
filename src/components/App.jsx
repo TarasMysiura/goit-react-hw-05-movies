7fa13bd64e1f7edb01b0c71e2845bad86cc6645c
TarasMysiura <tarasmysiura@gmail.com>
2023-07-19 10:45:15 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
+import HomePage from 'pages/HomePage';
+import React from 'react';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import styles from './App.module.css';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <nav className={styles.nav}>
+          <NavLink to="/">
+            <h2>Home</h2>{' '}
+          </NavLink>
+          <NavLink to="/movies">
+            <h2>Movies</h2>
+          </NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </main>
+      <footer></footer>
     </div>
   );
 };
+export default App;
